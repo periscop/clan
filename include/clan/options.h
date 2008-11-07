8a9bfa4511f4863fba18fc1fc626857f9cb6f9ee
bastoul <bastoul@d2347644-e8e6-0310-9099-9a715ae17836>
2008-11-07 13:51:53 +0000

    Merge with L-N branch... Loulou, can t you work directly in trunk...
    git-svn-id: https://alchemy.futurs.inria.fr/svn/users/bastoul/clan/trunk@16748 d2347644-e8e6-0310-9099-9a715ae17836

--- include/clan/options.h
+++ include/clan/options.h
@@ -59,9 +59,9 @@ struct clan_options
   int castle;    /**< 1 to put the Clan castle in output, 0 otherwise. */
   int structure; /**< 1 to print the clan_scop structure, 0 otherwise. */
   int inputscop; /**< 1 to read a .scop on the input, 0 to read a
-		      source file (default). */
+		    source file (default). */
   int arraystag; /**< 1 to dump the table of array names between the
-		      <arrays></arrays> tags, 0 otherwise (default). */
+		    <arrays></arrays> tags, 0 otherwise (default). */
 };
 typedef struct clan_options   clan_options_t;
 typedef struct clan_options * clan_options_p;
